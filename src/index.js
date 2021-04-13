76cfe65a3afd3ee51adae2677108b42cd6c9d862
Ahmed <Ahmed@LAPTOP-A6BDQO09>
2021-04-13 13:17:30 -0700

    Completed module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-export function fetchMovies() {}
+const movies = require('./data/movies.json');
+import { fetchWithTimeout } from "./services";
+export function fetchMovies() {
+    const resolveFunction = () => movies;
+    return fetchWithTimeout(setTimeout(() => {
+        
+    }, 1000)).then(fetchMovies);
+}
+const moviePromise = fetchMovies();
+moviePromise.then();
