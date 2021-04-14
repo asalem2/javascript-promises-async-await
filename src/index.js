c80c0eabe624d74b09a0142823aeb783a636de22
Ahmed <Ahmed@LAPTOP-A6BDQO09>
2021-04-13 22:40:36 -0700

    Completed module

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import { fetchWithTimeout } from "./services";
+import { fetchMovies } from "./services";
+import { fetchBooks } from "./services";
 const movies = require('./data/movies.json');
-export function fetchMovies() {
-    const resolveFunction = () => movies;
-    return fetchWithTimeout(1000).then(resolveFunction)
+function getBooksAndMovies() {
+    Promise.all([fetchBooks(), fetchMovies()]).then(([books, movies]) => ({
+        books, 
+        movies
+    }))
+    .catch(error => console.log("Error fetching books and movies", error));
 }
-const moviePromise = fetchMovies();
-moviePromise.then(results => {
-    console.log(results);
-});
